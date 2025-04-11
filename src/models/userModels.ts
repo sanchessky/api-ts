f65b60f38bafd0b1cdf4b55dbac43ac46cd65c33
Thiago Sanches <thiagomlkgame@gmail.com>
2025-04-11 14:26:02 -0300

    Documentação da Atividade

--- src/models/userModels.ts
+++ src/models/userModels.ts
@@ -1,5 +1,5 @@
 // importação da biblioteca do Row(linha)Data(dados)packet(Pacote). Guardar todos os dados que retorna da consulta select
-import { RowDataPacket } from "mysql2";
+import { RowDataPacket, ResultSetHeader } from "mysql2";
 // importação a conexão(connect) com o banco de dados para fazer a consulta nas tabelas do banco
 import connect from "../databese";
 
@@ -19,4 +19,23 @@ está função é do tipo ascíncrona e, portanto, aguardar um processesamento i
 export async function getAllUsers(): Promise<User[]> {
     const [rows] = await connect.query<User[]>('SELECT * FROM users', []);
     return rows;
+}
+
+/*A função do ResultSetHeader é fornecer informações sobre a execução de comandos SQL que não retornam dados, como:INSERT, UPDATE, DELETE, REPLACE
+Comandos DDL (CREATE TABLE, ALTER, etc.)
+Ou seja, o ResultSetHeader não contém dados da tabela, mas detalhes sobre o que aconteceu com o banco de dados após a execução da query.
+*/
+
+// Função para criar um novo usuário
+export async function createUser(user: Omit<User, 'id'>): Promise<ResultSetHeader> {
+    try {
+        const [result] = await connect.execute<ResultSetHeader>(
+            'INSERT INTO users (name, email) VALUES (?, ?)',
+            [user.name, user.email]
+        );
+        return result;
+    } catch (error) {
+        console.error('Erro ao criar usuário:', error);
+        throw error;
+    }
 }
